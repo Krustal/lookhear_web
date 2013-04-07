c7f11bbe34ae36a17728bb6f40e1ef052d6088f2
Byron McMullen <bmcmullen@whitepages.com>
2013-04-07 14:00:25 -0700

    auto setting of material type

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,4 +2,14 @@ class Material < ActiveRecord::Base
   attr_accessible :file, :title, :file_type
   belongs_to :tour
   mount_uploader :file, MaterialUploader
+
+  before_save :update_material_attributes
+
+  private
+
+  def update_material_attributes
+    if file.present? && file_changed?
+      self.file_type = file.file.content_type
+    end
+  end
 end
--- app/uploaders/material_uploader.rb
+++ app/uploaders/material_uploader.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
+require 'carrierwave/processing/mime_types'
 
 class MaterialUploader < CarrierWave::Uploader::Base
-
+  include CarrierWave::MimeTypes
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -38,9 +39,9 @@ class MaterialUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+     %w(jpg jpeg gif png m4a)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
