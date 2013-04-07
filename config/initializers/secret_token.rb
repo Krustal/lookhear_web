0a26b0f1d5271e95337597d4c12a891d7e12f63d
Byron McMullen <bmcmullen@whitepages.com>
2013-04-06 19:37:25 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lookhear::Application.config.secret_token = '6fd22d492bfb97c2213a9d206c5f4776151f8aadb9d6aa35c150a673f5568b22aaae499960099510fe14d022fa1e2684348cf9b02f9e382946b39be0c60a4298'
