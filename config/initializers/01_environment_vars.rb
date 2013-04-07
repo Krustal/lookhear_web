632a7eb8d69a9f6cdc7d5433dc3d98db0164d768
Byron McMullen <bmcmullen@whitepages.com>
2013-04-07 12:51:54 -0700

    removed dev env initializer

--- config/initializers/01_environment_vars.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env.test? || Rails.env.development?
-  ENV['AWS_ACCESS_KEY_ID'] = 'AKIAISLSU7SVRHOOJ3XQ'
-  ENV['AWS_SECRET_ACCESS_KEY'] = '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'
-  ENV['S3_BUCKET_NAME'] = 'lookhear'
-end
