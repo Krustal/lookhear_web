eb08df0e36786c3671f39f52d3a8f63d76002c60
Byron McMullen <bmcmullen@whitepages.com>
2013-04-07 15:56:38 -0700

    bootstrap cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   def index
   end
 
+  def after_sign_in_path_for(resource)
+    tours_path
+  end
+
   def private
     authenticate_user!
     render nothing: true
